4e6a3336a0be53d21285b9c5e567b54c591421ef
Peter Zernia <peterzernia@Peters-MacBook-Pro.local>
2018-10-13 15:16:17 +0200

    minor html changes

--- photo_blog/static/photo_blog/buttons.js
+++
@@ -1,9 +0,0 @@
-function toggleFollow(){
-      $.ajax({
-        url: "{% url 'user_follow' view.kwargs.username %}",
-        success: function(data) {
-          $("#followCount").html(data.follower_count + ' Followers');
-          $('#followElement').html(data.button);
-        }
-      });
-};
