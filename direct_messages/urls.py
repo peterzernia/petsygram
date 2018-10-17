e1c8f970a83735df0199412b3acf3fdd02565ae6
Peter Zernia <peterzernia@Peters-MacBook-Pro.local>
2018-10-17 15:04:19 +0200

    changed messaging to individual threads between users

--- direct_messages/urls.py
+++ direct_messages/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 from .views import (
-InboxView, SentView, CreateDirectMessage, DeleteDirectMessage, ViewDirectMessage
+InboxView, ThreadView, CreateDirectMessage, DeleteDirectMessage, ViewDirectMessage
 )
 
 
 urlpatterns = [
     path('inbox/', InboxView.as_view(), name='direct_messages-inbox'),
-    path('sent/', SentView.as_view(), name='direct_messages-sent'),
+    path('thread/<str:username>/', ThreadView.as_view(), name='direct_messages-thread'),
     path('new/', CreateDirectMessage.as_view(), name='direct_messages-new'),
     path('<int:pk>/', ViewDirectMessage.as_view(), name='direct_messages-detail'),
     path('<int:pk>/delete/', DeleteDirectMessage.as_view(), name='direct_messages-delete'),
--- direct_messages/views.py
+++ direct_messages/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_object_or_404
 from .models import DirectMessage
+from django.contrib.auth.models import User
 from django.views.generic import ListView, CreateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
+from itertools import chain
+from operator import attrgetter
 
 
 class InboxView(LoginRequiredMixin, ListView):
@@ -11,24 +14,28 @@ class InboxView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         direct_messages = DirectMessage.objects.filter(receiver=self.request.user)
-        direct_messages = direct_messages.order_by('-date_sent')
+        direct_messages = direct_messages.order_by('sender_id', '-date_sent')#.distinct('sender_id')
         return direct_messages
 
 
-class SentView(LoginRequiredMixin, ListView):
+class ThreadView(LoginRequiredMixin, ListView):
     model = DirectMessage
-    template_name = 'direct_messages/sent.html'
+    template_name = 'direct_messages/thread.html'
 
     def get_queryset(self):
-        direct_messages = DirectMessage.objects.filter(sender=self.request.user)
-        direct_messages = direct_messages.order_by('-date_sent')
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        dm1 = DirectMessage.objects.filter(receiver=user, sender=self.request.user)
+        dm2 = DirectMessage.objects.filter(receiver=self.request.user, sender=user)
+        direct_messages = sorted((chain(dm1, dm2)), key=attrgetter('date_sent'))
         return direct_messages
 
 
 class CreateDirectMessage(LoginRequiredMixin, CreateView):
     model = DirectMessage
     fields = ['receiver', 'content']
-    sucess_url = '/messages/sent/'
+
+    def get_success_url(self, **kwargs):
+        return reverse('direct_messages-inbox')
 
     def form_valid(self, form):
         form.instance.sender = self.request.user
@@ -54,5 +61,6 @@ class DeleteDirectMessage(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
-    def get_success_url(self):
-        return reverse('direct_messages-inbox')
+    def get_success_url(self, **kwargs):
+        username = self.object.receiver
+        return reverse( 'direct_messages-thread', args={username: 'username'})
