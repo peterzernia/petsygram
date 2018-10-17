8654087bab87e323dfd9e217135a80961b89b6db
Peter Zernia <peterzernia@Peters-MacBook-Pro.local>
2018-10-17 19:11:44 +0200

    homepage like button

--- photo_blog/static/photo_blog/buttons.js
+++ photo_blog/static/photo_blog/buttons.js
@@ -8,12 +8,12 @@ function toggleFollow(){
       });
 };
 
-function toggleLike(){
+function toggleLike(post_id){
       $.ajax({
         url: window.USER_LIKE_URL,
         success: function(data) {
-          $("#likeCount" + window.POST_ID).html(data.like_count + ' likes');
-          $("#imageElement" + window.POST_ID).html(data.img);
+          $("#likeCount" + post_id).html(data.like_count + ' likes');
+          $("#imageElement" + post_id).html(data.img);
           console.log(data);
         },
         error: function(error) {
