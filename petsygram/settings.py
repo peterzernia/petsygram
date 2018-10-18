265572d0432a98b1f1c6c175877f602de95e7f2e
Peter Zernia <peter176@gmail.com>
2018-10-18 12:12:01 +0200

    heroku updates

--- petsygram/settings.py
+++ petsygram/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
