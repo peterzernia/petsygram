16b179dc57fb13460978caa11cb9fe96c924bb9a
Peter Zernia <peter176@gmail.com>
2018-10-18 12:21:49 +0200

    heroku

--- petsygram/settings.py
+++ petsygram/settings.py
@@ -129,10 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
