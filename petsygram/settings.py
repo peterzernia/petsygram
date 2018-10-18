8d985013edb7164350ab986be130991a3462ec3d
Peter Zernia <peter176@gmail.com>
2018-10-18 11:10:31 +0200

    deleted __pycache__ and .DS_Store

--- petsygram/settings.py
+++ petsygram/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'petsygram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petsygram',
+        'USER': 'peterzernia',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
