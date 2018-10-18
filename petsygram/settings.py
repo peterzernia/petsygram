557625956934c3b2d7bc050b1b53d752217deee4
Peter Zernia <peter176@gmail.com>
2018-10-18 12:29:36 +0200

    heroku

--- petsygram/settings.py
+++ petsygram/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
