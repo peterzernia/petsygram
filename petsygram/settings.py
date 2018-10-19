3ee9899f7ed2fa63a6163c6ed7173446b0495b75
Peter Zernia <peter176@gmail.com>
2018-10-19 19:55:57 +0200

    fixed db in settings

--- petsygram/settings.py
+++ petsygram/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'petsygram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
-    }
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
