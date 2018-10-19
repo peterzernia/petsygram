fd7f41838ac31342d35cd31911db0da4cec0809d
Peter Zernia <43378846+peterzernia@users.noreply.github.com>
2018-10-19 19:38:10 +0200

    Update settings.py

--- petsygram/settings.py
+++ petsygram/settings.py
@@ -81,12 +81,9 @@ WSGI_APPLICATION = 'petsygram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'petsygram',
-        'USER': 'peterzernia',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
+    }
     }
 }
 
